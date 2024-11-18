9adbc863e2f7165c4901a2f71c9db58b9455133d
Mengqi Zhang <153735047+mqmqmqzzz@users.noreply.github.com>
2024-11-18 19:56:07 +0000

    Fixed the faults

--- test_zoo.py
+++ test_zoo.py
@@ -7,6 +7,15 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
        
     # Add your additional test cases here.
 
